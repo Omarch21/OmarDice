dfbf87bdd373471fd4b1ebb2578095d5bf64622f
Omar <89556037+Omarch21@users.noreply.github.com>
2022-02-05 22:13:36 -0600

    Delete DiceRoll.js

--- DiceRoll.js
+++
@@ -1,7 +0,0 @@
-function RollDiceOne() {
-    document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("diceTwo").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("diceThree").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("diceFour").value = Math.floor(Math.random()*6) + 1;
-    document.getElementById("diceFive").value = Math.floor(Math.random()*6) + 1;
-}
