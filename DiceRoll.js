1897a214a48f1a88af5548f6d12ad59c5291373d
Omar <89556037+Omarch21@users.noreply.github.com>
2022-02-05 21:47:42 -0600

    Update DiceRoll.js

--- DiceRoll.js
+++ DiceRoll.js
@@ -1,6 +1,3 @@
-// Try changing the capitalization of the the function name.
-// Try changing "x" to "X" in the difference calculation.
-
 function RollDiceOne() {
     document.getElementById("dice").value = Math.floor(Math.random()*6) + 1;
     document.getElementById("diceTwo").value = Math.floor(Math.random()*6) + 1;
